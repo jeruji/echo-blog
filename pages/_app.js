d11ca8999740306fd32d5ef48cd3ff3717efb183
jeruji <solar_medan@yahoo.com>
2021-05-12 22:06:35 +0700

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import ThemeProvider from 'providers/ThemeProvider';
+import { library, config } from '@fortawesome/fontawesome-svg-core';
+import { faSun, faMoon, faBorderAll, faList, faSortNumericDown, faSortNumericUp } from '@fortawesome/free-solid-svg-icons';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+config.autoAddCss = false;
+library.add(faSun, faMoon, faList, faBorderAll, faSortNumericDown, faSortNumericUp);
 
-export default MyApp
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'highlight.js/styles/darcula.css';
+import 'react-toggle/style.css';
+import 'styles/index.scss';
+
+export default ({Component, pageProps}) => 
+    <ThemeProvider>
+        <Component {...pageProps} />
+    </ThemeProvider>
